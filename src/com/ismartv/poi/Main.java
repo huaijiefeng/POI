11506abfacbb857ce940b45314e1bc556f15c2c1
fenghuibin <fenghuibin@ismartv.cn>
2014-07-22 10:03:34 +0800

    test

--- src/com/ismartv/poi/Main.java
+++ src/com/ismartv/poi/Main.java
@@ -1,5 +1,6 @@
 package com.ismartv.poi;
 
+import com.ismartv.poi.bean.DataBean;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -7,22 +8,37 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        InputStream is = new FileInputStream("/home/fenghb/Desktop/2.xlsx");
-        XSSFWorkbook xwb = new XSSFWorkbook(is);
-        XSSFSheet sheet = xwb.getSheetAt(0);
-        XSSFRow row;
-        String cell;
-        for (int i = sheet.getFirstRowNum(); i < sheet.getPhysicalNumberOfRows(); i++) {
-            row = sheet.getRow(i);
-            for (int j = row.getFirstCellNum(); j < row.getPhysicalNumberOfCells(); j++) {
-                cell = row.getCell(j).toString();
-                System.out.print(cell + "\t");
-            }
-            System.out.println("");
-        }
+    public static void main(String[] args) throws Exception {
+//        InputStream is = new FileInputStream("/home/fenghb/Desktop/3.xlsx");
+//        XSSFWorkbook xwb = new XSSFWorkbook(is);
+//        XSSFSheet sheet = xwb.getSheetAt(0);
+//        XSSFRow row;
+//        String cell;
+//        for (int i = sheet.getFirstRowNum(); i < sheet.getPhysicalNumberOfRows(); i++) {
+//            row = sheet.getRow(i);
+//            for (int j = row.getFirstCellNum(); j < row.getPhysicalNumberOfCells(); j++) {
+//                cell = row.getCell(j).toString();
+//                System.out.print(cell + "\t");
+//            }
+//            System.out.println("");
+//        }
+
+
+        ExampleEventUserModel example = new ExampleEventUserModel();
+        example.processOneSheet("/home/fenghb/Desktop/3.xlsx");
+
+
     }
+
+
+//    public List<DataBean> getListFromFile(InputStream inputStream) {
+//
+//        return null;
+//    }
+
+
 }
